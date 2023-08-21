334674f8bc8d8154a10691defd7f1e8badd9dc77
NigelOmondi <nigelomondi62@gmail.com>
2023-08-21 18:46:54 +0300

    Update DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from os import getenv
 
+class User(BaseModel, Base):
+    """This class defines a user by various attributes
+       Inherits from SQLAlchemy declarative_base
+       And links this class to Users table in MySQL
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+       Attributes:
+       __tablename__ (str): The name of the MySQL table to store users.
+       email: (sqlalchemy String): The user's email address.
+       password (sqlalchemy String): The user's password.
+       first_name (sqlalchemy String): The user's first name.
+       last_name (sqlalchemy String): The user's last name.
+       places (sqlalchemy relationship): The User-Place relationship.
+       reviews (sqlalchemy relationship): The User-Review relationship. 
+    """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
+    
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
