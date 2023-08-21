f679e6cdee3ae2f1e1d145397cc6ecd5ba3b8d27
Imukua <ianmukuaa@gmail.com>
2023-08-21 12:39:32 +0300

    implemented getter method for cities

--- models/state.py
+++ models/state.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import String, Column
+from models.city import City
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+	""" State class """
+	__tablename__ = "states"
+	name = Column(String(128, nullable=False))
+	cities = relationship("City", backref="state", cascade="delete")
+
+	@property
+	def cities(self):
+		"""Returns the list of City instances
+		with state_id equals to the current. """
+
+		allCities = models.storage.all(City)
+
+		return [city for city in allCities.values() if city.state_id == self.id]
