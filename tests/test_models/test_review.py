1f7756c4f58db4787e7a8b4665b176aad118fc36
NigelOmondi <nigelomondi62@gmail.com>
2023-08-23 18:30:04 +0300

    Add test cases

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,29 +1,86 @@
 #!/usr/bin/python3
-""" """
+"""
+Unittests for Reviews class.
+"""
+import unittest
+from os
+import pep8
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
+from models.base_model import BaseModel
 
 
-class test_review(test_basemodel):
-    """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "Review"
-        self.value = Review
+class TestReview(unittest.TestCase):
+    """Test the Review class"""
 
-    def test_place_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.place_id), str)
+    @classmethod
+    def setUpClass(cls):
+        """Test set up"""
+        cls.review = Review()
+        cls.review.place_id = "locationx"
+        cls.review.user_id = "guest10"
+        cls.review.text = "I highly recommended this place"
 
-    def test_user_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.user_id), str)
+    @classmethod
+    def teardown(cls):
+        """
+        To clean up after test is complete
+        Deleting "review" attribute from class
+        so as to reset test related review/data. 
+        """
+        del cls.review
 
-    def test_text(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.text), str)
+    def tearDown(self):
+        """
+        Is called after each test method.
+        Clean up any resources/temporary files
+        created during execution of test methods.
+        """
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_pep8_Review(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/review.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_checking_for_docstring_Review(self):
+        """Check for docstrings"""
+        self.assertIsNotNone(Review.__doc__)
+
+    def test_attributes_review(self):
+        """Check whether Review has attributes"""
+        self.assertTrue('id' in self.review.__dict__)
+        self.assertTrue('created_at' in self.review.__dict__)
+        self.assertTrue('updated_at' in self.review.__dict__)
+        self.assertTrue('place_id' in self.review.__dict__)
+        self.assertTrue('text' in self.review.__dict__)
+        self.assertTrue('user_id' in self.review.__dict__)
+
+    def test_is_subclass_Review(self):
+        """Test if review is a subclass of BaseModel"""
+        self.assertTrue(issubclass(self.review.__class__, BaseModel), True)
+
+    def test_attribute_types_Review(self):
+        """Test attribute type for Review"""
+        self.assertEqual(type(self.review.text), str)
+        self.assertEqual(type(self.review.place_id), str)
+        self.assertEqual(type(self.review.user_id), str)
+
+    @unittest.skipIf(getenv("HBNB_TYPE_STORAGE") == 'db', 'DB')
+    def test_save_Review(self):
+        """test if the save works"""
+        self.review.save()
+        self.assertNotEqual(self.review.created_at, self.review.updated_at)
+
+    def test_to_dict_Review(self):
+        """Test if the dictionary works"""
+        self.assertEqual('to_dict' in dir(self.review), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,34 +1,87 @@
 #!/usr/bin/python3
-""" """
+""" 
+Unittest for User class
+"""
+import unittest
+import os
+import pep8
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+from models.base_model import BaseModel
 
 
-class test_User(test_basemodel):
-    """ """
+class TestUser(unittest.TestCase):
+    """Test the User class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = "User"
-        self.value = User
+    @classmethod
+    def setUpClass(cls):
+        """Test set-up"""
+        cls.user = User()
+        cls.user.first_name = "Nigel"
+        cls.user.last_name = "Omondi"
+        cls.user.email = "nigelomondi62@gmail.com"
+        cls.user.password = "pa5sw0rd"
 
-    def test_first_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.first_name), str)
+    @classmethod
+    def teardown(cls):
+        """
+        To clean up after test is complete
+        Deleting "user" attribute from class
+        so as to reset test related user/data.  
+        """
+        del cls.user
 
-    def test_last_name(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.last_name), str)
+    def tearDown(self):
+        """
+        Is called after each test method.
+        Clean up any resources/temporary files
+        created during execution of test methods.
+        """
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
 
-    def test_email(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.email), str)
+    def test_pep8_User(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/user.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_password(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.password), str)
+    def test_checking_for_docstring_User(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(User.__doc__)
+
+    def test_attributes_User(self):
+        """chekcing if User have attributes"""
+        self.assertTrue('email' in self.user.__dict__)
+        self.assertTrue('id' in self.user.__dict__)
+        self.assertTrue('created_at' in self.user.__dict__)
+        self.assertTrue('updated_at' in self.user.__dict__)
+        self.assertTrue('password' in self.user.__dict__)
+        self.assertTrue('first_name' in self.user.__dict__)
+        self.assertTrue('last_name' in self.user.__dict__)
+
+    def test_is_subclass_User(self):
+        """test if User is subclass of Basemodel"""
+        self.assertTrue(issubclass(self.user.__class__, BaseModel), True)
+
+    def test_attribute_types_User(self):
+        """test attribute type for User"""
+        self.assertEqual(type(self.user.email), str)
+        self.assertEqual(type(self.user.password), str)
+        self.assertEqual(type(self.user.first_name), str)
+        self.assertEqual(type(self.user.first_name), str)
+
+    def test_save_User(self):
+        """test if the save works"""
+        self.user.save()
+        self.assertNotEqual(self.user.created_at, self.user.updated_at)
+
+    def test_to_dict_User(self):
+        """test if dictionary works"""
+        self.assertEqual('to_dict' in dir(self.user), True)
+
+
+if __name__ == "__main__":
+    unittest.main()
