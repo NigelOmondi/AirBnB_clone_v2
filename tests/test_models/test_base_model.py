79a2c48d4dc165bc51b3a74124ce7d383b07fc53
Imukua <ianmukuaa@gmail.com>
2023-08-23 22:45:55 +0300

    updated tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,99 +1,108 @@
 #!/usr/bin/python3
-""" """
-from models.base_model import BaseModel
-import unittest
-import datetime
-from uuid import UUID
-import json
+""" Defines unittests for models/base_model.py."""
 import os
+import pep8
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
-class test_basemodel(unittest.TestCase):
-    """ """
+class TestBaseModel(unittest.TestCase):
+    """tests  the BaseModel class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = 'BaseModel'
-        self.value = BaseModel
-
-    def setUp(self):
-        """ """
-        pass
-
-    def tearDown(self):
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up testing environment.
+        """
         try:
-            os.remove('file.json')
-        except:
+            os.rename("file.json", "tmp")
+        except IOError:
             pass
+        FileStorage._FileStorage__objects = {}
+        cls.storage = FileStorage()
+        cls.base = BaseModel()
 
-    def test_default(self):
-        """ """
-        i = self.value()
-        self.assertEqual(type(i), self.value)
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Teardown testing environment.
 
-    def test_kwargs(self):
-        """ """
-        i = self.value()
-        copy = i.to_dict()
-        new = BaseModel(**copy)
-        self.assertFalse(new is i)
+        """
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+        del cls.storage
+        del cls.base
 
-    def test_kwargs_int(self):
-        """ """
-        i = self.value()
-        copy = i.to_dict()
-        copy.update({1: 2})
-        with self.assertRaises(TypeError):
-            new = BaseModel(**copy)
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files("models/base_model.py")
+        self.assertEqual(result.total_errors, 0, "Fix pep8")
 
+    def test_docstring(self):
+        """test if docstring"""
+        self.assertIsNotNone(BaseModel.__doc__)
+
+    def test_attr(self):
+        """Check for BaseModel attributes."""
+        self.assertEqual(str, type(self.base.id))
+        self.assertEqual(datetime, type(self.base.created_at))
+        self.assertEqual(datetime, type(self.base.updated_at))
+
+    def test_allmethods(self):
+        """Check for all methods."""
+        self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "save"))
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
+        self.assertTrue(hasattr(BaseModel, "delete"))
+        self.assertTrue(hasattr(BaseModel, "__str__"))
+
+    def test_init(self):
+        """Test init method."""
+        self.assertIsInstance(self.base, BaseModel)
+
+    def test_unique(self):
+        """Test unique id."""
+        bm = BaseModel()
+        self.assertNotEqual(self.base.id, bm.id)
+        self.assertLess(self.base.created_at, bm.created_at)
+        self.assertLess(self.base.updated_at, bm.updated_at)
+
+    @unittest.skipIf(os.getenv("HBNB_ENV") is not None, "Testing DBStorage")
     def test_save(self):
-        """ Testing save """
-        i = self.value()
-        i.save()
-        key = self.name + "." + i.id
-        with open('file.json', 'r') as f:
-            j = json.load(f)
-            self.assertEqual(j[key], i.to_dict())
+        """Test save method."""
+        self.base.save()
+        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+        self.assertTrue(os.path.isfile("file.json"))
+        with open("file.json", "r") as f:
+            self.assertIn("BaseModel.{}".format(self.base.id), f.read())
+
+    def test_to_dict(self):
+        """Test to_dict method."""
+        testDict = self.base.to_dict()
+        self.assertEqual(self.base.id, testdict["id"])
+        self.assertEqual(self.base.created_at.isoformat(),
+                         testDict["created_at"])
+        self.assertEqual(self.base.updated_at.isoformat(),
+                         testDict["updated_at"])
+        self.assertEqual("__class__", testDict.keys())
+        self.assertEqual("BaseModel", testDict["__class__"])
+        self.assertEqual(testdict.get("_sa_instance_state", None), None)
 
     def test_str(self):
-        """ """
-        i = self.value()
-        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
-                         i.__dict__))
-
-    def test_todict(self):
-        """ """
-        i = self.value()
-        n = i.to_dict()
-        self.assertEqual(i.to_dict(), n)
-
-    def test_kwargs_none(self):
-        """ """
-        n = {None: None}
-        with self.assertRaises(TypeError):
-            new = self.value(**n)
-
-    def test_kwargs_one(self):
-        """ """
-        n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
-
-    def test_id(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.id), str)
-
-    def test_created_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.created_at), datetime.datetime)
-
-    def test_updated_at(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
-        n = new.to_dict()
-        new = BaseModel(**n)
-        self.assertFalse(new.created_at == new.updated_at)
+        """Test __str__ method."""
+        teststr = self.base.__str__()
+        self.assertIn("[BaseModel] ({})".format(self.base.id), teststr)
+        self.assertIn("'id': '{}'".format(self.base.id), teststr)
+        self.assertIn("'created_at': {}".format(
+            repr(self.base.created_at)), teststr)
+        self.assertIn("'updated_at': {}".format(
+            repr(self.base.updated_at)), teststr)
