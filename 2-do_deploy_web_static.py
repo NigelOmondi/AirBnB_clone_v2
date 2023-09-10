823eff3a76e7721989078c29e14c69419602704e
NigelOmondi <nigelomondi62@gmail.com>
2023-09-10 14:27:11 +0300

    Deploy archive files to servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,42 +9,39 @@ from datetime import datetime
 env.hosts = ['100.26.238.151', '100.25.183.127']
 
 def do_deploy(archive_path):
-    """Distributes an archive file to 2 web servers.
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
-    """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
+    """"Deploy an archive to web servers"""
+    if not exists(archive_path):
         return False
 
-    puts("New version deployed!")
-    return True
+    try:
+        """Upload archive to /tmp/"""
+        put(archive_path, "/tmp/")
+
+        """Get archive filename without extension"""
+        archive_filename = archive_path.split("/")[-1].split(".")[0]
+
+        """Create folder /data/web_static/releases/<archive_filename>"""
+        run("mkdir -p /data/web_static/releases/{}".format(archive_filename))
+
+        """Uncompress the archive"""
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}".format(
+           archive_filename, archive_filename))
+
+        """Remove the archive from /tmp/"""
+        run("rm /tmp/{}.tgz".format(archive_filename))
+
+        """Move the contents of the folder to its parent directory"""
+        run("mv /data/web_static/releases/{}/web_static/* \
+/data/web_static/releases/{}".format(archive_filename, archive_filename))
+
+        """Remove the old symbolic link"""
+        run("rm -rf /data/web_static/current")
+
+        """Create a new symbolic link"""
+        run("ln -s /data/web_static/releases/{} \
+             /data/web_static/current".format(archive_filename))
+
+        puts("New version deployed!")
+        return True
+    except Exception:
+        return False
