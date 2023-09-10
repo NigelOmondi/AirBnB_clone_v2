fdc7c7192af66688ee651c5f90d2b8853dc3ba1c
NigelOmondi <nigelomondi62@gmail.com>
2023-09-10 13:13:36 +0300

    Deploy archive files to servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,79 +1,48 @@
 #!/usr/bin/python3
-# Deploy archive
 """ Fabric script to distribute an archive to web servers"""
+
 from fabric.api import *
 from os.path import exists
 from os import path
 from datetime import datetime
 
 env.hosts = ['100.26.238.151', '100.25.183.127']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
-
-
-def do_pack():
-    """
-    Generate a .tgz archive from the contents of web_static
-    folder into a .tgz archive.
-    Returns:
-        Archive path if successful, None upon failure
-    """
-    try:
-        now = datetime.now()
-        timestamp = now.strftime("%Y%m%d%H%M%S")
-        archive_name = "web_static_" + timestamp + ".tgz"
-        local("mkdir -p versions")
-        local("tar -czvf versions/{} web_static".format(archive_name))
-        return "versions/{}".format(archive_name)
-    except Exception:
-        return None
-
 
 def do_deploy(archive_path):
-    """"Deploy an archive to web servers"""
-    if not exists(archive_path):
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    try:
-        """Upload archive to /tmp/"""
-        put(archive_path, "/tmp/")
-
-        """Get archive filename without extension"""
-        archive_filename = archive_path.split("/")[-1].split(".")[0]
-
-        """Create folder /data/web_static/releases/<archive_filename>"""
-        run("mkdir -p /data/web_static/releases/{}".format(archive_filename))
-
-        """Uncompress the archive"""
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}".format(
-           archive_filename, archive_filename))
-
-        """Remove the archive from /tmp/"""
-        run("rm /tmp/{}.tgz".format(archive_filename))
-
-        """Move the contents of the folder to its parent directory"""
-        run("mv /data/web_static/releases/{}/web_static/* \
-/data/web_static/releases/{}".format(archive_filename, archive_filename))
-
-        """Remove the old symbolic link"""
-        run("rm -rf /data/web_static/current")
-
-        """Create a new symbolic link"""
-        run("ln -s /data/web_static/releases/{} \
-             /data/web_static/current".format(archive_filename))
-
-        return True
-    except Exception:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-
-
-if __name__ == "__main__":
-    archive_path = do_pack()
-    if archive_path:
-        result = do_deploy(archive_path)
-        if result:
-            print("New version deployed!")
-        else:
-            print("Deployment failed.")
-    else:
-        print("Airchive creation failed")
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
